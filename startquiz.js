9ef592ae06d796e8b4b3b28da880302610f2362f
Usman Javed <usmanjaved0816@gmail.com>
2024-10-28 14:47:02 +0500

    complete previous and next button

--- startquiz.js
+++ startquiz.js
@@ -1,16 +1,24 @@
  // Retrieve quiz data from localStorage
  const quizData = JSON.parse(localStorage.getItem("quizData"));
+ let currentPage = 1;
+ let questionPerPage = 5;
 
  function displayQuiz(quizData) {
    const quizContainer = document.getElementById("quiz-questions");
    quizContainer.innerHTML = "";
 
-   quizData.forEach((questionData, index) => {
+//    calculate start and end index
+const startIndex = (currentPage - 1) * questionPerPage;
+const endIndex = Math.min(startIndex + questionPerPage, quizData.length);
+
+//  display only 5 question on current page 
+
+   quizData.slice(startIndex, endIndex).forEach((questionData, index) => {
      const questionHTML = `
          <div class="quiz-question-container" id="quiz-questions">
        <div class="quiz-question">
          <div class="question">
-           <h2>${index + 1}: ${questionData.question}</h2>
+           <h2>${startIndex + index + 1}: ${questionData.question}</h2>
          </div>
          <div class="options">
            ${[
@@ -20,8 +28,8 @@
              .sort(() => Math.random() - 0.5)
              .map(
                (option, idx) => `
-               <input type="radio" id="option${index}-${idx}" name="quiz${index}" value="${option}" />
-               <label for="option${index}-${idx}" class="radio-label">${option}</label>`
+               <input type="radio" id="option${startIndex + index}-${idx}" name="quiz${startIndex + index}" value="${option}" />
+               <label for="option${startIndex + index}-${idx}" class="radio-label">${option}</label>`
              )
              .join("")}
          </div>
@@ -31,7 +39,26 @@
      `;
      quizContainer.innerHTML += questionHTML;
    });
+
+//    update button showing
+document.getElementById('prev-btn').style.display = currentPage === 1 ? 'none' : 'inline'
+document.getElementById('next-btn').style.display = endIndex >= quizData.length ? 'none':'inline';
  }
+//  addEventListner for next and prev button 
+
+document.getElementById('next-btn').addEventListener('click' , ()=> {
+    if(currentPage * questionPerPage < quizData.length) {
+        currentPage++;
+        displayQuiz(quizData);
+    }
+});
+
+document.getElementById('prev-btn').addEventListener('click', ()=> {
+    if (currentPage > 1) {
+        currentPage--;
+        displayQuiz(quizData);
+    }
+});
 
  if (quizData) {
    displayQuiz(quizData);
@@ -39,6 +66,8 @@
    document.getElementById("quiz-questions").innerText =
      "No quiz data available.";
  }
+
+
 
 
 
