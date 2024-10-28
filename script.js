accd4a53703d0f6259e28228f9e0e16d033acba7
Usman Javed <usmanjaved0816@gmail.com>
2024-10-28 11:08:23 +0500

    quiz question access dynamically

--- script.js
+++ script.js
@@ -89,7 +89,3 @@ quizzes.forEach((quiz) => {
 
 
 
-
-// 
-
-
---
+++ startquiz.js
@@ -0,0 +1,39 @@
+ // Retrieve quiz data from localStorage
+ const quizData = JSON.parse(localStorage.getItem("quizData"));
+
+ function displayQuiz(quizData) {
+   const quizContainer = document.getElementById("quiz-questions");
+   quizContainer.innerHTML = "";
+
+   quizData.forEach((questionData, index) => {
+     const questionHTML = `
+       <div class="quiz-question">
+         <div class="question">
+           <h2>${index + 1}: ${questionData.question}</h2>
+         </div>
+         <div class="options">
+           ${[
+             ...questionData.incorrect_answers,
+             questionData.correct_answer,
+           ]
+             .sort(() => Math.random() - 0.5)
+             .map(
+               (option, idx) => `
+               <input type="radio" id="option${index}-${idx}" name="quiz${index}" value="${option}" />
+               <label for="option${index}-${idx}" class="radio-label">${option}</label>`
+             )
+             .join("")}
+         </div>
+       </div>
+       <hr />
+     `;
+     quizContainer.innerHTML += questionHTML;
+   });
+ }
+
+ if (quizData) {
+   displayQuiz(quizData);
+ } else {
+   document.getElementById("quiz-questions").innerText =
+     "No quiz data available.";
+ }
